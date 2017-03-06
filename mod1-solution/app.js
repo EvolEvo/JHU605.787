c11d194e175595f75887427558a4cb7baef97f80
Jessica Cassidy <bellaxstrega@gmail.com>
2017-03-05 20:52:31 -0500

    updating

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -2,6 +2,30 @@
 'use strict';
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', function ($scope) {
+	$scope.menu = "";
+	$scope.message = "This is the message";
+	$scope.button = "";
 
-});
+/*	$scope.displayMessage = function() {
+		var resultMessage = determineResult($scope.menu);
+		$scope.message = resultMessage;
+
+	};
+
+	function determineResult(string) {
+		var result = "";
+		var items[];
+		items = string.split();
+		if (items.length() < 1) {
+			result = "Please enter data first";
+		}
+		else if (items.length() >= 1 && items.length() <= 3){
+			result = "Enjoy!";
+		}
+		else {
+			result = "Too much!";
+		}
+		return result;
+	}*
+}); */
 })();
