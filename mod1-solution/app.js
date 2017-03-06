41a4b63157a8977e339d333b10962c0de96a8f22
Cassidy <cassija1@jhuapl.edu>
2017-03-06 13:59:18 -0500

    adding and updating files

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -1,31 +1,32 @@
 (function () {
 'use strict';
+
 angular.module('LunchCheck', [])
+
 .controller('LunchCheckController', function ($scope) {
 	$scope.menu = "";
-	$scope.message = "This is the message";
+	$scope.response = "This is the message";
 	$scope.button = "";
 
-/*	$scope.displayMessage = function() {
+	$scope.displayMessage = function() {
 		var resultMessage = determineResult($scope.menu);
-		$scope.message = resultMessage;
+		$scope.response = resultMessage;
 
 	};
 
 	function determineResult(string) {
 		var result = "";
-		var items[];
-		items = string.split();
-		if (items.length() < 1) {
+		var items = string.split(",");
+		if (items.length< 1) {
 			result = "Please enter data first";
 		}
-		else if (items.length() >= 1 && items.length() <= 3){
+		else if (items.length >= 1 && items.length <= 3){
 			result = "Enjoy!";
 		}
 		else {
 			result = "Too much!";
 		}
 		return result;
-	}*
-}); */
+	}
+}); 
 })();
