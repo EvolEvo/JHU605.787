79edbd65e90d488e5f9f843d97887f6531320a0c
EvolEvo <bellaxstrega@gmail.com>
2017-03-14 13:59:09 -0400

    updating

--- mod2-solution/app.js
+++ mod2-solution/app.js
@@ -4,25 +4,54 @@
 angular.module('ShoppingListCheckOff', [])
 .controller('ToBuyController', ToBuyController)
 .controller('AlreadyBoughtController', AlreadyBoughtController)
-.service('ShoppingListCheckOffService', ShoppingListService);
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService) {
-	var showToBuy = this;
-	showToBuy.items = ShoppingListCheckOffService.getItems();
+	var toBuy = this;
+	toBuy.items = ShoppingListCheckOffService.getToBuy();
+	toBuy.buy = function (item) {
+		ShoppingListCheckOffService.buy(item);
+	}
 }
 
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService) {
 
-	var showAlreadyBought = this;
-	showAlreadyBought.items = ShoppingListCheckOffService.getItems();
+	var alreadyBought = this;
+	alreadyBought.items = ShoppingListCheckOffService.getAlreadyBought();
 }
 
 function ShoppingListCheckOffService() {
+	var service = this;
 
-	var toBuy = [];
-	var bought = [];
+	var toBuy = [
+		{name: "Cookies", pricePerItem: .50, quantity: 10},
+		{name: "Chicken", pricePerItem: 2.49, quantity: 2},
+		{name: "Eggs", pricePerItem: 2.20, quantity: 1},
+		{name: "Milk", pricePerItem: 3.49, quantity: 1},
+		{name: "Bread", pricePerItem: 3.99, quantity: 2}
+	];
+	var alreadyBought = [];
+
+	service.getToBuy = function () {
+		return toBuy;
+	};
+
+	service.getAlreadyBought = function() {
+		return alreadyBought;
+	};
+
+	service.buy = function(item) {
+		var itemIndex = toBuy.indexOf(item);
+		toBuy.splice(itemIndex, 1);
+		alreadyBought.push(item);
+
+	};
+
+	service.calculateTotal = function(item) {
+		return item.pricePerItem * item.quantity;
+	}
 
 //	service.addItem = function (itemName, quantity)
 }
