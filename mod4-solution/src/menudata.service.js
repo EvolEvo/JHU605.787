f0e9e66681fd3b5b4177c524743d163e106191dc
EvolEvo <bellaxstrega@gmail.com>
2017-04-17 12:22:56 -0400

    updating

--- mod4-solution/src/menudata.service.js
+++ mod4-solution/src/menudata.service.js
@@ -16,16 +16,14 @@ function MenuDataService($http) {
       	return result.data;
 		});
 	};
-	/*service.getAllCategories = function() {
-		return $http.get('https://davids-restaurant.herokuapp.com/categories.json');
-	}; */
+	
 
 	service.getItemsForCategory = function(categoryShortName) {
 		return $http({
       	method: "GET",
       	url: ("https://davids-restaurant.herokuapp.com/menu_items.json?category=" + categoryShortName)
     	}).then(function (result) {
-      	return result.data.menu_items;
+      	return result.data;
 		});
 
 	};
